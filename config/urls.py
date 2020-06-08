c87b9c64c0859b1a9ac550afb33571345f0378f0
VenumWolf <venumthewofl@gmail.com>
2020-06-08 14:46:48 -0500

    Added router to root

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from rest_framework import routers
 
 from config.settings.base import ADMIN_SITE_PATH
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
+    path("", include(router.urls)),
     path(ADMIN_SITE_PATH, admin.site.urls),
 ]
