378b7effb7b2c9ee7e9097f0c3e8979be3c67169
VenumWolf <venumthewofl@gmail.com>
2020-06-09 23:33:17 -0500

    Fixed testing setup

---
+++ config/settings/test.py
@@ -0,0 +1,30 @@
+from .base import *
+
+# GENERAL
+
+SECRET_KEY = '*piusv42ywk&=dy$xc$qmk$vch17zo2-kzxi0+d4o($yvy4wv-'
+
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
+    }
+}
+
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+TEMPLATES[-1]["OPTIONS"]["loaders"] = [
+    (
+        "django.template.loaders.cached.Loader",
+        [
+            "django.template.loaders.filesystem.Loader",
+            "django.template.loaders.app_directories.Loader",
+        ],
+    )
+]
+
+# EMAIL
+
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
