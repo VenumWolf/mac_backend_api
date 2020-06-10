41c6613496f1aef66c9743f587dab79d4a0feb1a
VenumWolf <venumthewofl@gmail.com>
2020-06-10 12:06:21 -0500

    Mapped the "api/" path to the API router

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from rest_framework import routers
 
 from config.settings.base import ADMIN_SITE_PATH
 
-router = routers.DefaultRouter()
-
 urlpatterns = [
-    path("", include(router.urls)),
     path(ADMIN_SITE_PATH, admin.site.urls),
+    path("api/", include("config.api_router")),
 ]
