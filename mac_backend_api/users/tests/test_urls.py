c5e2e642d36f78a05cab63b27c2aff0363662cbe
VenumWolf <venumthewofl@gmail.com>
2020-06-11 20:02:35 -0500

    Refactored url tests, fix mistakes, and use mixer instead

--- mac_backend_api/users/tests/test_urls.py
+++ mac_backend_api/users/tests/test_urls.py
@@ -1,23 +1,19 @@
 import pytest
 from django.urls import resolve, reverse
-
-from mac_backend_api.users.models import User
-
-pytestmark = pytest.mark.djang_db
+from mixer.backend.django import mixer
 
 
-def test_user_detail(user: User):
-    assert (
-        reverse("api:user-detail", kwargs={"username": user.username}) == f"/api/users/{user.username}/"
-    )
-    assert resolve("f/api/users/{user.username}/").view_name == "api:user-detail"
+@pytest.mark.django_db
+class TestUrls:
+    def test_user_detail(self):
+        user = mixer.blend("users.User")
+        assert reverse("api:user-detail", kwargs={"username": user.username}) == f"/api/users/{user.username}/"
+        assert resolve(f"/api/users/{user.username}/").view_name == "api:user-detail"
 
+    def test_user_list(self):
+        assert reverse("api:user-list") == "/api/users/"
+        assert resolve("/api/users/").view_name == "api:user-list"
 
-def test_user_list():
-    assert reverse("api:user-list") == "/api/users/"
-    assert resolve("/api/users/").view_name == "api:user-list"
-
-
-def test_user_me():
-    assert reverse("api:user-me") == "/api/users/me/"
-    assert resolve("/api/users/me/").view_name == "api:user-me"
+    def test_user_me(self):
+        assert reverse("api:user-me") == "/api/users/me/"
+        assert resolve("/api/users/me/").view_name == "api:user-me"
