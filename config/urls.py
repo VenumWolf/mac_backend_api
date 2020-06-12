4bde07aff6d21117f591100653269edea9adb593
VenumWolf <venumthewofl@gmail.com>
2020-06-12 08:43:15 -0500

    Redirect root to /api/

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.views.generic import RedirectView
 
 from config.settings.base import ADMIN_SITE_PATH
 
 urlpatterns = [
     path(ADMIN_SITE_PATH, admin.site.urls),
     path("api/", include("config.api_router")),
+    path("", RedirectView.as_view(url="/api/"), name="index")
 ]
