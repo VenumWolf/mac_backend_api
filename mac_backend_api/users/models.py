2b20440b65f383d5ddf48e24acc1a448a485cf93
VenumWolf <venumthewofl@gmail.com>
2020-06-11 22:03:38 -0500

    Implemented get_absolute_url for user model

--- mac_backend_api/users/models.py
+++ mac_backend_api/users/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from uuid import uuid4
 
+from django.urls import reverse
+
+
 class User(AbstractUser):
     id = models.UUIDField(
         primary_key=True,
         default=uuid4,
         editable=False)
     name = models.CharField(
-        help_text="Name of the user", 
-        blank=True, 
+        help_text="Name of the user",
+        blank=True,
         max_length=255)
+
+    def get_absolute_url(self) -> str:
+        return reverse("api:user-detail", kwargs={"username": self.username})
+
