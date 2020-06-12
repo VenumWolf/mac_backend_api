91bb91c796dc47b84eaf19d1843551c0f0761b1f
VenumWolf <venumthewofl@gmail.com>
2020-06-12 08:25:09 -0500

    Created the custom UserAdmin

--- mac_backend_api/users/admin.py
+++ mac_backend_api/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from mac_backend_api.users.forms import UserChangeForm, UserCreationForm
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
+    list_display = ["username", "name", "is_superuser"]
+    search_fields = ["name"]
