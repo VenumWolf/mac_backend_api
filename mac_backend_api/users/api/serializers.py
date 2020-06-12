e18b5dc6189aa939fef76919ecaf59d9f5375735
VenumWolf <venumthewofl@gmail.com>
2020-06-12 07:39:46 -0500

    Added url to user serializer

--- mac_backend_api/users/api/serializers.py
+++ mac_backend_api/users/api/serializers.py
@@ -6,7 +6,7 @@ from mac_backend_api.users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "email", "name"]
+        fields = ["username", "email", "name", "url"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
