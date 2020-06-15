0f06622ed2a6afb454e52aabf2e27df31c03cc51
VenumWolf <venumthewofl@gmail.com>
2020-06-15 17:17:59 -0500

    Updated auth api endpoints

--- config/urls.py
+++ config/urls.py
@@ -5,14 +5,15 @@ from django.views.generic import RedirectView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-)
+    TokenVerifyView)
 
 from config.settings.base import ADMIN_SITE_PATH
 
 urlpatterns = [
     path(ADMIN_SITE_PATH, admin.site.urls),
-    path('api/authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("api/authentication/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/", include("config.api_router")),
     path("", RedirectView.as_view(url="/api/"), name="index")
 ]
