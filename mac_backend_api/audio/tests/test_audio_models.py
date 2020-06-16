fe8771c46afb1e56e391872ffacf2970e7836c74
VenumWolf <venumthewofl@gmail.com>
2020-06-16 14:25:51 -0500

    User stuff is no longer needed here

--- mac_backend_api/audio/tests/test_audio_models.py
+++ mac_backend_api/audio/tests/test_audio_models.py
@@ -13,10 +13,6 @@ User = get_user_model()
 class TestAudioModel(TestCase):
     def setUp(self) -> None:
         self.audio = mixer.blend(Audio)
-        self.audio_author_user_1 = mixer.blend(User)
-        self.audio_author_1 = mixer.blend(Author, user=self.audio_author_user_1, audio=self.audio)
-        self.audio_author_user_2 = mixer.blend(User)
-        self.audio_author_2 = mixer.blend(Author, user=self.audio_author_user_2, audio=self.audio)
 
     def test_get_url(self) -> None:
         assert self.audio.get_absolute_url() == f"/api/audio/{self.audio.id}/"
