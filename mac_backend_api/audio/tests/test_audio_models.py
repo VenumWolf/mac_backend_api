73f840b743a0151748a6dd52e8f168af52792bd2
VenumWolf <venumthewofl@gmail.com>
2020-06-18 12:58:06 -0500

    Cover subsequent calls to add/remove like methods

--- mac_backend_api/audio/tests/test_audio_models.py
+++ mac_backend_api/audio/tests/test_audio_models.py
@@ -44,10 +44,25 @@ class TestAudioModel(TestCase):
         self.audio.add_like(user)
         assert self.audio.like_set.get(user=user).user == user
 
+    def test_add_double_like(self):
+        """Ensures subsequent attempts to like don't raise exceptions"""
+        user = mixer.blend(User)
+        self.audio.add_like(user)
+        self.audio.add_like(user)
+        assert self.audio.like_set.get(user=user).user == user
+
     def test_remove_like(self):
         """Ensures likes are removed correctly"""
         user = mixer.blend(User)
         self.audio.add_like(user)
+        self.audio.remove_like(user)
+        assert len(self.audio.like_set.filter(user=user)) == 0
+
+    def test_remove_nonexistent_like(self):
+        """Ensures subsequent attempts to unlike don't raise exceptions"""
+        user = mixer.blend(User)
+        self.audio.add_like(user)
+        self.audio.remove_like(user)
         self.audio.remove_like(user)
         assert len(self.audio.like_set.filter(user=user)) == 0
 
