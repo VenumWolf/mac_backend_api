c0e82b60680b528f8a8a2a0fde0994e9047dc3ed
VenumWolf <venumthewofl@gmail.com>
2020-06-18 07:30:48 -0500

    Commented tests

--- mac_backend_api/audio/tests/test_audio_models.py
+++ mac_backend_api/audio/tests/test_audio_models.py
@@ -14,22 +14,17 @@ class TestAudioModel(TestCase):
         self.__blend_audio_and_author()
         self.like_user = mixer.blend(User)
 
-    def __blend_audio_and_author(self) -> None:
-        self.audio = mixer.blend(Audio)
-        self.author = mixer.blend(Author)
-        self.user = mixer.blend(User)
-        self.audio.authors.add(self.author)
-        self.audio.views = 0
-        self.audio.save()
-
     def test_get_url(self) -> None:
+        """Ensures the audio url is what is expected"""
         assert self.audio.get_absolute_url() == f"/api/audio/{self.audio.id}/"
 
     def test_add_listen(self) -> None:
+        """Ensures the Audio's add_listen method adds 1 listen"""
         self.audio.add_listen()
         assert self.audio.listen_count == 1
 
     def test_like_count(self) -> None:
+        """Ensures the Audio's like_count property finds and counts its likes"""
         like = mixer.blend(Like)
         like.user = self.like_user
         like.audio = self.audio
@@ -38,5 +33,14 @@ class TestAudioModel(TestCase):
         like.delete()
 
     def test_like_count_with_other_likes_present(self):
+        """Ensures the Audio's like_count property only counts likes referencing itself"""
         mixer.blend(Like)
         assert self.audio.like_count == 0
+
+    def __blend_audio_and_author(self) -> None:
+        self.audio = mixer.blend(Audio)
+        self.author = mixer.blend(Author)
+        self.user = mixer.blend(User)
+        self.audio.authors.add(self.author)
+        self.audio.views = 0
+        self.audio.save()
