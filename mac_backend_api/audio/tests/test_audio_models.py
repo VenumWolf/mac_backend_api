cce9fea9c6822594bfb5fe3d1635e13dbd86df4f
VenumWolf <venumthewofl@gmail.com>
2020-06-18 07:13:25 -0500

    Cleaned up test code, added test_like_count

--- mac_backend_api/audio/tests/test_audio_models.py
+++ mac_backend_api/audio/tests/test_audio_models.py
@@ -3,8 +3,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from mixer.backend.django import mixer
 
-from mac_backend_api.audio.models import Audio, Author
-
+from mac_backend_api.audio.models import Audio, Author, Like
 
 User = get_user_model()
 
@@ -12,8 +11,16 @@ User = get_user_model()
 @pytest.mark.django_db
 class TestAudioModel(TestCase):
     def setUp(self) -> None:
+        self.__blend_audio_and_author()
+        self.like_user = mixer.blend(User)
+
+    def __blend_audio_and_author(self) -> None:
         self.audio = mixer.blend(Audio)
+        self.author = mixer.blend(Author)
+        self.user = mixer.blend(User)
+        self.audio.authors.add(self.author)
         self.audio.views = 0
+        self.audio.save()
 
     def test_get_url(self) -> None:
         assert self.audio.get_absolute_url() == f"/api/audio/{self.audio.id}/"
@@ -21,3 +28,11 @@ class TestAudioModel(TestCase):
     def test_add_listen(self) -> None:
         self.audio.add_listen()
         assert self.audio.listen_count == 1
+
+    def test_like_count(self) -> None:
+        like = mixer.blend(Like)
+        like.user = self.like_user
+        like.audio = self.audio
+        like.save()
+        assert self.audio.like_count == 1
+        like.delete()
