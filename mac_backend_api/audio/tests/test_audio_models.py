4dcfd4ed5bc466848647fccf833780297b9cf1bc
VenumWolf <venumthewofl@gmail.com>
2020-06-18 12:49:28 -0500

    Create tests for add and remove like methods

--- mac_backend_api/audio/tests/test_audio_models.py
+++ mac_backend_api/audio/tests/test_audio_models.py
@@ -38,6 +38,19 @@ class TestAudioModel(TestCase):
         mixer.blend(Like)
         assert self.audio.like_count == 0
 
+    def test_add_like(self):
+        """Ensures likes are added correctly"""
+        user = mixer.blend(User)
+        self.audio.add_like(user)
+        assert self.audio.like_set.get(user=user).user == user
+
+    def test_remove_like(self):
+        """Ensures likes are removed correctly"""
+        user = mixer.blend(User)
+        self.audio.add_like(user)
+        self.audio.remove_like(user)
+        assert len(self.audio.like_set.filter(user=user)) == 0
+
     def __blend_audio_and_author(self) -> None:
         self.audio = mixer.blend(Audio)
         self.author = mixer.blend(Author)
