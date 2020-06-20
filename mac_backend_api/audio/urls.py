d89dc77af9fd68c37c3e648168a5dcbf28afd253
VenumWolf <venumthewofl@gmail.com>
2020-06-20 08:41:18 -0500

    Set audio urls

--- mac_backend_api/audio/urls.py
+++ mac_backend_api/audio/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from mac_backend_api.audio.api.views import AudioListView, AudioDetailView
+
 app_name = 'audio'
 
 urlpatterns = [
-
+    path("<uuid:id>", AudioDetailView.as_view(), name="audio-detail"),
+    path("", AudioListView.as_view(), name="audio-list")
 ]
