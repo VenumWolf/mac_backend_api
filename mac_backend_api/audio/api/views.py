6d811a705fc30547d9d34d5d96cacd5796dfc155
VenumWolf <venumthewofl@gmail.com>
2020-06-20 08:41:04 -0500

    Created generic views

--- mac_backend_api/audio/api/views.py
+++ mac_backend_api/audio/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, UpdateModelMixin
 from rest_framework.viewsets import GenericViewSet
+from rest_framework import generics
 
 from mac_backend_api.audio.api.serializers import AudioSerializer
 from mac_backend_api.audio.models import Audio
@@ -9,3 +10,16 @@ class AudioViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, Generic
     serializer_class = AudioSerializer
     queryset = Audio.objects.filter(is_public=True)
     lookup_field = "id"
+
+
+class AudioBaseView:
+    queryset = Audio.objects.filter(is_public=True)
+    serializer_class = AudioSerializer
+
+
+class AudioListView(AudioBaseView, generics.ListCreateAPIView):
+    pass
+
+
+class AudioDetailView(AudioBaseView, generics.RetrieveUpdateDestroyAPIView):
+    pass
