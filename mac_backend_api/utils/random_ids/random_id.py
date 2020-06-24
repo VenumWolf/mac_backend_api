8f7cbce12bbf17765e8c8d776c90b7c16fed44ba
VenumWolf <venumthewofl@gmail.com>
2020-06-23 20:15:54 -0500

    Small refactoring

--- mac_backend_api/utils/random_ids/random_id.py
+++ mac_backend_api/utils/random_ids/random_id.py
@@ -1,16 +1,17 @@
 import string
 from random import SystemRandom
 
+URLSAFE_SPECIALS = "-._~"
+
 DEFAULT_ID_LENGTH = 14
-SAFE_SPECIALS = "-._~"
-DEFAULT_CHARACTER_SET = string.ascii_letters + string.digits + SAFE_SPECIALS
+DEFAULT_CHARACTER_SET = string.ascii_letters + string.digits + URLSAFE_SPECIALS
 
 
 def random_id(length=DEFAULT_ID_LENGTH, character_set=DEFAULT_CHARACTER_SET):
     """
     Generate a random ID with the given length and character_set.
     :param length:        The length of the id
-    :param character_set: The selection of characters to make up the id
+    :param character_set: A string, list, or a tuple containing the characters from which to create the ID
     :return:              A random ID of the given length and character set
     """
     if length < 1: raise ValueError("Length must be at least 1")
