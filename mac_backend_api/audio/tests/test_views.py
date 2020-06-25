05ff2524bda497f5426c3d2d1da1d52a578dceb6
VenumWolf <venumthewofl@gmail.com>
2020-06-25 00:08:36 -0500

    Completed audio viewset tests

--- mac_backend_api/audio/tests/test_views.py
+++ mac_backend_api/audio/tests/test_views.py
@@ -21,13 +21,40 @@ class TestAudioViewSet(TestCase):
     def test_detail_get(self):
         response = self.client.get(reverse("api:audio-detail", kwargs={"id": self.audio.id}))
         assert response.status_code == 200
-        assert response.data == {
-            "id": str(self.audio.id),
-            "title": self.audio.title,
-            "slug": self.audio.slug,
-            "description": self.audio.description,
-            "listen_count": self.audio.listen_count,
-            "uploaded_at": self.audio.uploaded_at.strftime(settings.DATETIME_FORMAT),
-            "is_public": self.audio.is_public,
-            "url": f"http://testserver{self.audio.get_absolute_url()}"
+        TestAudioViewSet.__verify_audio_matches_data(self.audio, response.data)
+
+    def test_detail_put(self):
+        response = self.client.put(
+            reverse("api:audio-detail", kwargs={"id": self.audio.id}),
+            data="title=Hello%2C%20World%21",
+            content_type="application/x-www-form-urlencoded",
+        )
+        assert response.status_code == 200
+        assert Audio.objects.get(id=self.audio.id).title == "Hello, World!"
+
+    def test_detail_patch(self):
+        response = self.client.put(
+            reverse("api:audio-detail", kwargs={"id": self.audio.id}),
+            data="title=Hello%2C%20World%21",
+            content_type="application/x-www-form-urlencoded",
+        )
+        assert response.status_code == 200
+        assert Audio.objects.get(id=self.audio.id).title == "Hello, World!"
+
+    def test_detail_delete(self):
+        audio = mixer.blend(Audio)
+        response = self.client.delete(reverse("api:audio-detail", kwargs={"id": audio.id}))
+        assert response.status_code == 204
+
+    @staticmethod
+    def __verify_audio_matches_data(audio, data):
+        assert data == {
+            "id": str(audio.id),
+            "title": audio.title,
+            "slug": audio.slug,
+            "description": audio.description,
+            "listen_count": audio.listen_count,
+            "uploaded_at": audio.uploaded_at.strftime(settings.DATETIME_FORMAT),
+            "is_public": audio.is_public,
+            "url": f"http://testserver{audio.get_absolute_url()}"
         }
