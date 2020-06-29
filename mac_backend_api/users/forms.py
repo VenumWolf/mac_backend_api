4de9f89434afd61d4cc935535955f42f72463504
VenumWolf <venumthewofl@gmail.com>
2020-06-29 13:13:39 -0500

    Apply GPL license

--- mac_backend_api/users/forms.py
+++ mac_backend_api/users/forms.py
@@ -1,3 +1,20 @@
+#  Copyright (C) 2020  Mind Audio Central
+#
+#  This file is part of mac_backend_api.
+#
+#  mac_backend_api is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  mac_backend_api is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with mac_backend_api.  If not, see <https://www.gnu.org/licenses/>.
+
 from django.contrib.auth import forms, get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
--- mac_backend_api/users/tests/test_views.py
+++ mac_backend_api/users/tests/test_views.py
@@ -1,3 +1,20 @@
+#  Copyright (C) 2020  Mind Audio Central
+#
+#  This file is part of mac_backend_api.
+#
+#  mac_backend_api is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  mac_backend_api is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with mac_backend_api.  If not, see <https://www.gnu.org/licenses/>.
+
 import pytest
 from django.test import TestCase, RequestFactory
 from mixer.backend.django import mixer
