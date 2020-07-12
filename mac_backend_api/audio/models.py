cd2d979365f365568c3fd2e4ffd855cc28898311
VenumWolf <venumthewofl@gmail.com>
2020-07-12 13:34:52 -0500

    Moved classes and functions from fields.py to the AudioStream model

--- mac_backend_api/audio/models.py
+++ mac_backend_api/audio/models.py
@@ -157,6 +157,27 @@ def get_audio_stream_upload_path(stream, filename) -> str:
 
 class AudioStream(models.Model):
 
+    class AudioFormat(models.TextChoices):
+        MP3 = "mp3"
+        AAC = "aac"
+        OGG = "ogg"
+        WAV = "wav"
+
+    class AudioBitRate(models.IntegerChoices):
+        MINIMAL = 32000
+        VERY_LOW = 96000
+        LOW = 128000
+        AVERAGE = 192000
+        HIGH = 256000
+        VERY_HIGH = 320000
+        MAXIMUM = 512000
+
+    class AudioSampleRate(models.IntegerChoices):
+        LOW = 44100
+        AVERAGE = 48000
+        HIGH = 88200
+        VERY_HIGH = 96000
+
     id = models.UUIDField(
         primary_key=True,
         default=uuid4,
@@ -191,6 +212,15 @@ class AudioStream(models.Model):
         upload_to=get_audio_stream_upload_path,
         help_text="The stream's audio file, it will be processed to match the format and bit_rate values"
     )
+
+    @staticmethod
+    def is_valid_extension(extension) -> bool:
+        """
+        Returns True if a given file extension is a supported format.
+        :param extension: The file extension to check.
+        :return:          True if the file extension is that of a supported audio format.
+        """
+        return extension.lower() in [extension[0] for extension in AudioFormat.choices]
 
 
 class Like(models.Model):
