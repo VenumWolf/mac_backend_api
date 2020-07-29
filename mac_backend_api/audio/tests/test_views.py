313af6e41369d94d30f889ba8aa55af71314cb4c
VenumWolf <venumthewofl@gmail.com>
2020-07-29 06:17:37 -0500

    Removed old tests, instead test AudioViewSet direcectly

--- mac_backend_api/audio/tests/test_views.py
+++ mac_backend_api/audio/tests/test_views.py
@@ -19,85 +19,45 @@ from django.conf import settings
 from django.test import TestCase, Client
 from django.urls import reverse
 from mixer.backend.django import mixer
+from rest_framework.test import APIRequestFactory
 
+from mac_backend_api.audio.api.views import AudioViewSet
 from mac_backend_api.audio.models import Audio, Stream
+
+
+def blend_audio(count=1):
+    """
+    A helper method for creating audios.
+    :param count: The number of audios to blend.
+    :return:      An Audio instance if count = 1, or a list of Audio instances if count > 1.
+    """
+    if count == 1:
+        audio = mixer.blend(Audio)
+    elif count > 1:
+        audio = list()
+        for i in range(count):
+            audio.append(mixer.blend(Audio))
+    else:
+        audio = list()
+    return audio
 
 
 class TestAudioViewSet(TestCase):
     def setUp(self) -> None:
-        self.audio = mixer.blend(Audio, is_public=True)
-        self.stream = mixer.blend(Stream, audio=self.audio)
-        self.client = Client()
+        self.view_set = AudioViewSet
+        self.request_factory = APIRequestFactory()
 
-    def test_list(self):
-        response = self.client.get(reverse("api:audio-list"))
-        assert response.status_code == 200
-        assert len(response.data) == 1
+    def test_get_detail_view(self) -> None:
+        """Verifies the AudioViewSet's detail view returns with status code 200 when provided a valid id."""
+        audio = blend_audio()
+        request = self.request_factory.get("")
+        audio_detail_view = self.view_set.as_view({"get": "retrieve"})
+        response = audio_detail_view(request, id=audio.id)
+        self.assertEquals(response.status_code, 200)
 
-    def test_detail_get(self):
-        response = self.client.get(reverse("api:audio-detail", kwargs={"id": self.audio.id}))
-        assert response.status_code == 200
-        TestAudioViewSet.__verify_audio_matches_data(self.audio, response.data)
-
-    def test_detail_put(self):
-        response = self.client.put(
-            reverse("api:audio-detail", kwargs={"id": self.audio.id}),
-            data="title=Hello%2C%20World%21",
-            content_type="application/x-www-form-urlencoded",
-        )
-        assert response.status_code == 200
-        assert Audio.objects.get(id=self.audio.id).title == "Hello, World!"
-
-    def test_detail_patch(self):
-        response = self.client.put(
-            reverse("api:audio-detail", kwargs={"id": self.audio.id}),
-            data="title=Hello%2C%20World%21",
-            content_type="application/x-www-form-urlencoded",
-        )
-        assert response.status_code == 200
-        assert Audio.objects.get(id=self.audio.id).title == "Hello, World!"
-
-    def test_detail_delete(self):
-        audio = mixer.blend(Audio)
-        response = self.client.delete(reverse("api:audio-detail", kwargs={"id": audio.id}))
-        assert response.status_code == 204
-
-    @staticmethod
-    def __verify_audio_matches_data(audio, data):
-        assert data.get("id") == str(audio.id)
-        assert data.get("title") == audio.title
-        assert data.get("slug") == audio.slug
-        assert data.get("description") == audio.description
-        assert data.get("listen_count") == audio.listen_count
-        assert data.get("uploaded_at") == audio.uploaded_at.strftime(settings.DATETIME_FORMAT)
-        assert data.get("is_public") == audio.is_public
-        assert data.get("url") == f"http://testserver{audio.get_absolute_url()}"
-        assert data.get("streams") is not None  # Audio tests do not need to verify this, but it should never be None.
-
-
-class TestStreamViewSet(TestCase):
-    def setUp(self) -> None:
-        self.audio = mixer.blend(Audio)
-        self.stream = mixer.blend(Stream, audio=self.audio)
-        self.client = Client()
-
-    def test_list(self):
-        response = self.client.get(reverse("api:stream-list"))
-        assert response.status_code == 200
-        assert len(response.data) == 1
-
-    def test_detail_get(self):
-        response = self.client.get(reverse("api:stream-detail", kwargs={"id": self.stream.id}))
-        assert response.status_code == 200
-        TestStreamViewSet.__verify_stream_matches_data(self.stream, response.data)
-
-    @staticmethod
-    def __verify_stream_matches_data(stream, data):
-        assert data.get("id") == str(stream.id)
-        assert data.get("url") == f"http://testserver{stream.get_absolute_url()}"
-        assert data.get("audio") == f"http://testserver{stream.audio.get_absolute_url()}"
-        assert data.get("format") == stream.format
-        assert data.get("bit_rate") == stream.bit_rate
-        assert data.get("sample_rate") == stream.sample_rate
-        assert data.get("allow_downloads") == stream.allow_downloads
-        assert data.get("file") == f"http://testserver/media/{stream.file.name}"
+    def test_get_detail_view_invalid_id(self) -> None:
+        """Verifies the AudioViewSet's detail view returns a 404 when provided an invalid id."""
+        request = self.request_factory.get("")
+        audio_detail_view = self.view_set.as_view({"get": "retrieve"})
+        response = audio_detail_view(request, id="invalid")
+        self.assertEquals(response.status_code, 404)
