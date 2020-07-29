8eb885ea025253800d9f381620b514b1d1768de4
VenumWolf <venumthewofl@gmail.com>
2020-07-29 05:47:53 -0500

    Moved audio url tests to test_global_urls.py

--- mac_backend_api/tests/test_global_urls.py
+++ mac_backend_api/tests/test_global_urls.py
@@ -15,7 +15,35 @@
 #  You should have received a copy of the GNU General Public License
 #  along with mac_backend_api.  If not, see <https://www.gnu.org/licenses/>.
 
-from django.urls import reverse
+import pytest
+from django.urls import resolve, reverse
+from mixer.backend.django import mixer
+
+from mac_backend_api.audio.models import Audio, Stream
+
+
+@pytest.mark.django_db
+class TestAudioUrls:
+    def test_audio_detail(self):
+        audio = mixer.blend(Audio)
+        assert reverse("api:audio-detail", kwargs={"id": audio.id}) == f"/api/audio/{audio.id}/"
+        assert resolve(f"/api/audio/{audio.id}/").view_name == "api:audio-detail"
+
+    def test_audio_list(self):
+        assert reverse("api:audio-list") == "/api/audio/"
+        assert resolve("/api/audio/").view_name == "api:audio-list"
+
+
+@pytest.mark.django_db
+class TestStreamUrls:
+    def test_stream_detail(self):
+        stream = mixer.blend(Stream)
+        assert reverse("api:stream-detail", kwargs={"id": stream.id}) == f"/api/stream/{stream.id}/"
+        assert resolve(f"/api/stream/{stream.id}/").view_name == "api:stream-detail"
+
+    def test_audio_list(self):
+        assert reverse("api:stream-list") == "/api/stream/"
+        assert resolve("/api/stream/").view_name == "api:stream-list"
 
 
 class TestAuthUrls:
