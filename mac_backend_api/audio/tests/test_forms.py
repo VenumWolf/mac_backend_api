780682680af249e04aa13c71161025292472ffc4
VenumWolf <venumthewofl@gmail.com>
2020-08-04 06:57:30 -0500

    Created test base for AudioCreationForm

---
+++ mac_backend_api/audio/tests/test_forms.py
@@ -0,0 +1,35 @@
+#  Copyright (C) 2020  Mind Audio Central
+#
+#  This file is part of mac_backend_api.
+#
+#  mac_backend_api is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  mac_backend_api is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with mac_backend_api.  If not, see <https://www.gnu.org/licenses/>.
+from io import BytesIO
+
+from django.test import TestCase
+
+from mac_backend_api.audio.forms import AudioCreationForm
+
+
+class TestAudioCreationForm(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def get_form(self, title="Test", description="Test", is_public=True, file=BytesIO(b"content")):
+        file.name = "test_file.mp3"
+        return AudioCreationForm({
+            "title": title,
+            "description": description,
+            "is_public": is_public,
+            "file": file
+        })
