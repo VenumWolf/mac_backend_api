dd2474418df555da8f36ff56b85486a608a155b2
VenumWolf <venumthewofl@gmail.com>
2020-08-03 19:25:02 -0500

    Moved user url tests to global tests

--- mac_backend_api/tests/test_urls.py
+++ mac_backend_api/tests/test_urls.py
@@ -23,6 +23,22 @@ from mac_backend_api.audio.models import Audio, Stream
 
 
 @pytest.mark.django_db
+class TestUrls:
+    def test_user_detail(self):
+        user = mixer.blend("users.User")
+        assert reverse("api:user-detail", kwargs={"username": user.username}) == f"/api/users/{user.username}/"
+        assert resolve(f"/api/users/{user.username}/").view_name == "api:user-detail"
+
+    def test_user_list(self):
+        assert reverse("api:user-list") == "/api/users/"
+        assert resolve("/api/users/").view_name == "api:user-list"
+
+    def test_user_me(self):
+        assert reverse("api:user-me") == "/api/users/me/"
+        assert resolve("/api/users/me/").view_name == "api:user-me"
+
+
+@pytest.mark.django_db
 class TestAudioUrls:
     def test_audio_detail(self):
         audio = mixer.blend(Audio)
