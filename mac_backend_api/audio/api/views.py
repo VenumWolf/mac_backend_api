72d9255a4ff3b5b44727bb0ab729a426aeda175b
VenumWolf <venumthewofl@gmail.com>
2020-08-09 17:12:09 -0500

    Implemented custom create method

--- mac_backend_api/audio/api/views.py
+++ mac_backend_api/audio/api/views.py
@@ -15,21 +15,42 @@
 #  You should have received a copy of the GNU General Public License
 #  along with mac_backend_api.  If not, see <https://www.gnu.org/licenses/>.
 
+from rest_framework import status
+from rest_framework.decorators import parser_classes
+from rest_framework.exceptions import ErrorDetail
+from rest_framework.parsers import MultiPartParser, FileUploadParser, JSONParser, FormParser
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from mac_backend_api.audio.api.serializers import AudioSerializer, StreamSerializer
 from mac_backend_api.audio.models import Audio, Stream
 
 
+ERROR_DATA = {
+    "file_not_provided": {
+            "detail": ErrorDetail("File was not provided"),
+            "code": "file_not_provided"
+    }
+}
+
+
 class AudioViewSet(ModelViewSet):
     serializer_class = AudioSerializer
     lookup_field = "id"
     queryset = Audio.objects.all()
+    parser_classes = (MultiPartParser, FormParser, JSONParser)
 
     def filter_queryset(self, queryset):
         if self.action == "list":
             queryset = queryset.filter(is_public=True)
         return queryset
+
+    def create(self, request, *args, **kwargs):
+        if request.FILES.get("file") is None:
+            response = Response(data=ERROR_DATA.get("file_not_provided"), status=status.HTTP_400_BAD_REQUEST)
+        else:
+            response = super(AudioViewSet, self).create(request, args, kwargs)
+        return response
 
 
 class StreamViewSet(ModelViewSet):
