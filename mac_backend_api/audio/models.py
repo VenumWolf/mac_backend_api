ce0ecabe911319d555c6e2df4993899a2760530b
VenumWolf <venumthewofl@gmail.com>
2020-08-09 23:42:29 -0500

    Updated audio formats to match standards a bit better

--- mac_backend_api/audio/models.py
+++ mac_backend_api/audio/models.py
@@ -149,19 +149,17 @@ class Stream(models.Model):
         WAV = "wav"
 
     class AudioBitRate(models.IntegerChoices):
-        MINIMAL = 32000
-        VERY_LOW = 96000
-        LOW = 128000
-        AVERAGE = 192000
-        HIGH = 256000
-        VERY_HIGH = 320000
-        MAXIMUM = 512000
+        MINIMUM = 32000
+        LOW = 64000
+        AVERAGE = 96000
+        HIGH = 128000
+        VERY_HIGH = 192000
+        MAXIMUM = 256000
 
     class AudioSampleRate(models.IntegerChoices):
         LOW = 44100
         AVERAGE = 48000
-        HIGH = 88200
-        VERY_HIGH = 96000
+        HIGH = 96000
 
     id = models.UUIDField(
         primary_key=True,
