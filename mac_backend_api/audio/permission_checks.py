1a76e6ab481a90df886f81f3cf94056fe5486208
VenumWolf <venumthewofl@gmail.com>
2020-08-16 17:53:22 -0500

    Created custom permissions checks class

---
+++ mac_backend_api/audio/permission_checks.py
@@ -0,0 +1,25 @@
+#  Copyright (C) 2020  Mind Audio Central
+#
+#  This file is part of mac_backend_api.
+#
+#  mac_backend_api is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  mac_backend_api is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with mac_backend_api.  If not, see <https://www.gnu.org/licenses/>.
+
+from rest_framework.permissions import BasePermission, IsAuthenticatedOrReadOnly
+
+from mac_backend_api.audio.models import Audio, Stream
+
+
+class IsOwnerOrReadOnly(IsAuthenticatedOrReadOnly):
+    def has_object_permission(self, request, view, obj):
+        pass
