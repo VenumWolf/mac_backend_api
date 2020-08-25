43ea60f02397f29ecb090fbcf37fcc6d4b5db889
VenumWolf <venumthewofl@gmail.com>
2020-08-25 13:30:57 -0500

    Updated User model

--- mac_backend_api/users/models.py
+++ mac_backend_api/users/models.py
@@ -15,17 +15,18 @@
 #  You should have received a copy of the GNU General Public License
 #  along with mac_backend_api.  If not, see <https://www.gnu.org/licenses/>.
 
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-from uuid import uuid4
-
+from django.db import models
 from django.urls import reverse
+
+from mac_backend_api.utils.random_id.random_id import random_id
 
 
 class User(AbstractUser):
-    id = models.UUIDField(
+    id = models.CharField(
         primary_key=True,
-        default=uuid4,
+        max_length=14,
+        default=random_id,
         editable=False)
     name = models.CharField(
         help_text="Name of the user",
