6c788e61fe8bcb3d4b48f352f0917a3eba80dbb4
VenumWolf <venumthewofl@gmail.com>
2020-09-17 11:32:37 -0500

    Fixed documentation

--- mac_backend_api/audio/utils/audio_processing.py
+++ mac_backend_api/audio/utils/audio_processing.py
@@ -41,31 +41,36 @@ def identify_file_format(file, identifier=default_file_identifier) -> str:
     :param identifier: A function which takes a file-like object and returns it's type as a string.  Refer to the
                        specific identification function's documentation for more informatoin.
                        (Default: default_file_identifier())
-
     :return:           A string containing the format name (ex. mp3, ogg, wav, ext.)
     """
     return identifier(file)
 
 
 def pydub_audio_processor(file, **kwargs):
-    output_file = SpooledTemporaryFile()
+    """
+    Convert and reformat audio using the Pydub library.
+    :param file:   A file, or bytes-like object containing the unprocessed audio data.
+    :param kwargs: Parameters passed to Pydub's AudioSegment.export() method (see its documentation for more info.)
+    :return:       A file-like object containing the processed audio data.
+    """
     output_format = kwargs.get("format")
     file_format = identify_file_format(file)
     file.seek(0)
     audio = AudioSegment.from_file(file, file_format)
     audio.set_frame_rate(kwargs.get("sample_rate", 48000))
-    audio.export(output_file, format=output_format)
+    output_file = audio.export(format=output_format)
+    output_file.seek(0)
     return output_file
 
 
 def process_audio(file, processor=pydub_audio_processor, **kwargs):
     """
     Convert and reformat audio using a set converter.
-    :param file:       The input file to process.
+    :param file:      The input file to process.
     :param processor: A function which will process the audio (reference the processor function's
-                       documentation for more information.)
-    :param kwargs:     The arguments to pass to the processors (reference the processor function's documentation for more
-                       information.)
-    :return:           A file-like object containing the processed audio data.
+                      documentation for more information.)
+    :param kwargs:    The arguments to pass to the processors (reference the processor function's documentation for
+                      more information.)
+    :return:          A file-like object containing the processed audio data.
     """
     return processor(file, **kwargs)
